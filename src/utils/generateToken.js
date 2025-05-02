ed535fc80ed868ddced2ef7338a9dc733077606d
Asero <codevincent624@gmail.com>
2025-05-02 21:12:10 +0800

    update generate token dont include password in JWT token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const generateToken = (user) => {
-  const { user_id, email,password } = user;
-  return jwt.sign({ user_id, email,password }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const { user_id, email } = user;
+  return jwt.sign({ user_id, email }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 module.exports = { generateToken };
