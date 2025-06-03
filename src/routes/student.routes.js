857138c926bd0f00494604bf74132832fdc2c5ff
Quivir Cutanda <quivir.c@codero.ph>
2025-06-03 14:33:49 +0800

    implement update student info

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -9,7 +9,9 @@ const { viewStudentECards} = require('../controllers/student/eCard.controller');
 const { viewAllStudents } = require('../controllers/student/viewAllStudents.controller');
 const { validatePagination } = require('../middleware/paginationValidation');
 const { getStudentFullDetails } = require('../controllers/student/studentDetails.controller');
-const { validateStudentRegistration, validateStudentSearch, validateLRN, validateStudentId } = require('../middleware/studentValidation');
+const { updateStudent } = require('../controllers/student/updateStudent.controller');
+const { validateStudentRegistration, validateStudentSearch, validateLRN, validateStudentId,validateStudentUpdate } = require('../middleware/studentValidation');
+
 const { uploadSF10: uploadSF10Handler } = require('../controllers/student/uploadSF10.controller');
 
 router.post('/register', authenticate, checkPermission('register_student'), validateStudentRegistration, addStudent);
@@ -18,5 +20,6 @@ router.get('/all', authenticate, checkPermission('view_student_info'), validateP
 router.get('/:lrn/details', authenticate, checkPermission('view_student_info'), validateLRN, getStudentFullDetails);
 router.get('/:studentId/ecards', authenticate, checkPermission('view_ecards'), validateStudentId, viewStudentECards);
 router.post('/upload-sf10/:studentId',authenticate, uploadSF10, uploadSF10Handler);
+router.put('/:lrn/update', authenticate, checkPermission('update_student_info'), validateLRN, validateStudentUpdate, updateStudent);
 
 module.exports = router;
