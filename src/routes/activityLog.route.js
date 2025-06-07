fe0b3b544a839cf3b34dfc874d7d4710fb7e30fc
Quivir Cutanda <quivir.c@codero.ph>
2025-06-08 06:28:40 +0800

    activity-log-by-user

--- src/routes/activityLog.route.js
+++ src/routes/activityLog.route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const authenticate = require('../middleware/authMiddleware');
 const authorizePermission = require('../middleware/authorizePermission');
 const { validatePagination } = require('../middleware/paginationValidation');
-const { viewActivityLogs } = require('../controllers/activityLog/viewActivityLogs.controller');
+const { viewActivityLogs, viewUserActivityLogs } = require('../controllers/activityLog/viewActivityLogs.controller');
 
 router.get('/', authenticate, authorizePermission('view_logs'), validatePagination, viewActivityLogs);
+
+router.get('/:userId', authenticate, authorizePermission('view_logs'), validatePagination, viewUserActivityLogs);
 
 module.exports = router;
